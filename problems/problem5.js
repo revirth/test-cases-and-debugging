40ccd31797982b55bda623fe0431bbf7778a19fa
JT <revirth@gmail.com>
2019-04-05 12:55:09 -0400

    problem5

--- problems/problem5.js
+++ problems/problem5.js
@@ -1,27 +1,34 @@
-let verifyEquals = require('./verify-equals.js');
+let verifyEquals = require("./verify-equals.js");
 
-// we need 5 test cases. 
-let inputs = [
-  [2, 7]
-]
+// we need 5 test cases.
+let inputs = [[2, 7], ["a", 7], [verifyEquals, 7], [1, 2, 3, 4], 444];
 
-let outputs = [
-  14
-]
+let outputs = [14, undefined, undefined, undefined, undefined];
 
 /*
 The input of the function is an array.
-Make this function return the product of the two numbers in the array. If one of the numbers is not passed, or if anything other than numbers are passed, return undefined.
+Make this function return the product of the two numbers in the array. 
+If one of the numbers is not passed, or if anything other than numbers are passed, return undefined.
 */
 function f(input) {
-    
+  if (
+    Array.isArray(input) === false ||
+    input.length !== 2 ||
+    input.some(n => isNaN(n))
+  )
+    return undefined;
+
+  return input.reduce((a, b) => a * b);
 }
 
 function runTest(i) {
-    if(i > inputs.length) throw new Error("You do not have enough test cases");
-    let expected = outputs[i];
-    let actual = f(inputs[i]);
-    verifyEquals(expected, actual)
+  if (i > inputs.length) throw new Error("You do not have enough test cases");
+  let expected = outputs[i];
+  let actual = f(inputs[i]);
+
+  console.log(actual, expected, actual === expected);
+
+  verifyEquals(expected, actual);
 }
 
 runTest(0);
@@ -29,4 +36,4 @@ runTest(1);
 runTest(2);
 runTest(3);
 runTest(4);
-console.log("All tests passed for " + __filename)
+console.log("All tests passed for " + __filename);
