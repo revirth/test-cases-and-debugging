6c1d3fd8c875f43b43fa2fdb704d0b5e9181c3ed
JT <revirth@gmail.com>
2019-04-05 14:38:15 -0400

    problem11

--- problems/problem11.js
+++ problems/problem11.js
@@ -1,26 +1,28 @@
-let verifyEquals = require('./verify-equals.js');
+let verifyEquals = require("./verify-equals.js");
 
-// we need 5 test cases. 
-let inputs = [
-  
-]
+// we need 5 test cases.
+let inputs = [[], [6, 6, 6], ["a", 6, verifyEquals, 6, 6], Array(3).fill(6), 0];
 
-let outputs = [
-  
-]
+let outputs = [0, 18, 18, 18, 0];
 
 /*
-Make this function return the sum of all the numbers in the input array. If any element in the array is not a number, skip it. If the array is empty, return zero.
+Make this function return the sum of all the numbers in the input array. If any element in the array is not a number, skip it. 
+If the array is empty, return zero.
 */
 function f(arr) {
-    
+  if (Array.isArray(arr) === false || arr.length === 0) return 0;
+
+  return arr.filter(n => !isNaN(n)).reduce((a, b) => a + b);
 }
 
 function runTest(i) {
-    if(i > inputs.length) throw new Error("You do not have enough test cases");
-    let expected = outputs[i];
-    let actual = f(inputs[i]);
-    verifyEquals(expected, actual)
+  if (i > inputs.length) throw new Error("You do not have enough test cases");
+  let expected = outputs[i];
+  let actual = f(inputs[i]);
+
+  console.log(actual, expected);
+
+  verifyEquals(expected, actual);
 }
 
 runTest(0);
@@ -29,4 +31,4 @@ runTest(2);
 runTest(3);
 runTest(4);
 
-console.log("All tests passed for " + __filename)
+console.log("All tests passed for " + __filename);
