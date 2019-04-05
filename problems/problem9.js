a688ad5607704296e337bc8a9e782e8052a9a304
JT <revirth@gmail.com>
2019-04-05 14:32:18 -0400

    problem9

--- problems/problem9.js
+++ problems/problem9.js
@@ -1,13 +1,15 @@
-let verifyEquals = require('./verify-equals.js');
+let verifyEquals = require("./verify-equals.js");
 
-// we need 5 test cases. 
+// we need 5 test cases.
 let inputs = [
-  
-]
+  "hey hello morning",
+  "",
+  "The quick brown fox jumped over the lazy dog",
+  "May the force be with you",
+  "a b c d "
+];
 
-let outputs = [
-  
-]
+let outputs = ["morning", "", "jumped", "force", "d"];
 
 /*
 Make this function return the longest word in the input string. If the input string is empty then return an empty string.
@@ -21,14 +23,20 @@ HINTS:
    - A for loop might be helpful
 */
 function f(str) {
-    
+  return str
+    .trim()
+    .split(" ")
+    .reduce((a, b) => (a.length > b.length ? a : b));
 }
 
 function runTest(i) {
-    if(i > inputs.length) throw new Error("You do not have enough test cases");
-    let expected = outputs[i];
-    let actual = f(inputs[i]);
-    verifyEquals(expected, actual)
+  if (i > inputs.length) throw new Error("You do not have enough test cases");
+  let expected = outputs[i];
+  let actual = f(inputs[i]);
+
+  console.log(actual, expected);
+
+  verifyEquals(expected, actual);
 }
 
 runTest(0);
@@ -37,4 +45,4 @@ runTest(2);
 runTest(3);
 runTest(4);
 
-console.log("All tests passed for " + __filename)
+console.log("All tests passed for " + __filename);
