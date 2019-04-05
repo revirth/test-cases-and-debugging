466b352df17a748a55e248eb663594475363e602
JT <revirth@gmail.com>
2019-04-05 14:52:55 -0400

    problem13

--- problems/problem13.js
+++ problems/problem13.js
@@ -1,29 +1,37 @@
-let verifyEquals = require('./verify-equals.js');
+let verifyEquals = require("./verify-equals.js");
 
-// we need 5 test cases. 
-let inputs = [
-  
-]
+// we need 5 test cases.
+let inputs = ["RADAR", "JAVASCRIPT", "", "ab", "amanaplanacanalpanama"];
 
-let outputs = [
-  
-]
+let outputs = [true, false, undefined, false, true];
 
 /*
-Make this function return true if the input string is a palindrome, and false otherwise. A palindrome is simply a string that is the same if you reverse it.
+Make this function return true if the input string is a palindrome, and false otherwise. 
+A palindrome is simply a string that is the same if you reverse it.
 
 RADAR -> Yes
 JAVASCRIPT -> No
 */
 function f(str) {
-    
+  if (typeof str !== "string" || str.trim().length === 0) return undefined;
+
+  return (
+    str ===
+    str
+      .split("")
+      .reverse()
+      .join("")
+  );
 }
 
 function runTest(i) {
-    if(i > inputs.length) throw new Error("You do not have enough test cases");
-    let expected = outputs[i];
-    let actual = f(inputs[i]);
-    verifyEquals(expected, actual)
+  if (i > inputs.length) throw new Error("You do not have enough test cases");
+  let expected = outputs[i];
+  let actual = f(inputs[i]);
+
+  console.log(actual, expected);
+
+  verifyEquals(expected, actual);
 }
 
 runTest(0);
@@ -32,4 +40,4 @@ runTest(2);
 runTest(3);
 runTest(4);
 
-console.log("All tests passed for " + __filename)
+console.log("All tests passed for " + __filename);
