a33d0f6209d55a1065c309320b80150ae62fc4b7
JT <revirth@gmail.com>
2019-04-05 11:57:27 -0400

    problem2

--- problems/problem2.js
+++ problems/problem2.js
@@ -1,23 +1,24 @@
-let verifyEquals = require('./verify-equals.js');
+let verifyEquals = require("./verify-equals.js");
 
 // we need 5 test cases.
-let inputs = [
-
-]
-
-let outputs = [
-  
-]
+let inputs = ["ab", "   b", " * f ? ", "     ", "123809184"];
+let outputs = ["b", "b", "f", undefined, undefined];
 
 // Make this function return the last letter of the string that is passed to it. If the string does not have a last letter, return undefined
 function f(str) {
-    
+  return str
+    .split("")
+    .reverse()
+    .filter(x => x.toLowerCase() !== x.toUpperCase())[0];
 }
 
 function runTest(i) {
-    let expected = outputs[i];
-    let actual = f(inputs[i]);
-    verifyEquals(expected, actual)
+  let expected = outputs[i];
+  let actual = f(inputs[i]);
+
+  console.log(actual, expected, actual === expected);
+
+  verifyEquals(expected, actual);
 }
 
 runTest(0);
@@ -25,4 +26,4 @@ runTest(1);
 runTest(2);
 runTest(3);
 runTest(4);
-console.log("All tests passed for " + __filename)
+console.log("All tests passed for " + __filename);
