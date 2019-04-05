aa78d77856ac503fc1840698da2e68664d30b52d
JT <revirth@gmail.com>
2019-04-05 15:26:01 -0400

    problem14

--- problems/problem14.js
+++ problems/problem14.js
@@ -1,13 +1,21 @@
-let verifyEquals = require('./verify-equals.js');
+let verifyEquals = require("./verify-equals.js");
 
-// we need 5 test cases. 
+// we need 5 test cases.
 let inputs = [
-  
-]
+  "Lorem ipsumos dolor sit amet consectetur adipisicing elit. Magni quisquam",
+  "----------------------------------------",
+  "",
+  "a b c d e f g h i j k l m n o p q r s tu v w x y z a b c d e f g h i j k l m n op q r s tu v w x y z",
+  verifyEquals
+];
 
 let outputs = [
-  
-]
+  "Lorem ipsumos dolor sit amet consectetur\nadipisicing elit. Magni quisquam",
+  "----------------------------------------",
+  "",
+  "a b c d e f g h i j k l m n o p q r s tu\nv w x y z a b c d e f g h i j k l m n op\nq r s tu v w x y z",
+  undefined
+];
 
 /*
 Make this function return the input string wrapped to 40 characters per line. 
@@ -31,14 +39,30 @@ Lorem ipsumos dolor sit amet consectetur
 even though there is a space before the a in adipisicing
 */
 function f(str) {
-    
+  if (typeof str !== "string") return undefined;
+
+  if (str.length <= 40) return str;
+
+  let ret = [];
+
+  while (str !== "") {
+    ret.push(str.substr(0, 40));
+    str = str.substr(41);
+
+    console.log(str);
+  }
+
+  return ret.join("\n");
 }
 
 function runTest(i) {
-    if(i > inputs.length) throw new Error("You do not have enough test cases");
-    let expected = outputs[i];
-    let actual = f(inputs[i]);
-    verifyEquals(expected, actual)
+  if (i > inputs.length) throw new Error("You do not have enough test cases");
+  let expected = outputs[i];
+  let actual = f(inputs[i]);
+
+  console.warn(actual, expected);
+
+  verifyEquals(expected, actual);
 }
 
 runTest(0);
@@ -47,4 +71,4 @@ runTest(2);
 runTest(3);
 runTest(4);
 
-console.log("All tests passed for " + __filename)
+console.log("All tests passed for " + __filename);
