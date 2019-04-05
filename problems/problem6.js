a602c6ceab76defba02cb0f5fda93d11455ed184
JT <revirth@gmail.com>
2019-04-05 13:05:57 -0400

    problem6

--- problems/problem6.js
+++ problems/problem6.js
@@ -1,15 +1,17 @@
 // pro tip: use nodemon instead of node
-let verifyEquals = require('./verify-equals.js');
+let verifyEquals = require("./verify-equals.js");
 
-// we need 6 test cases. 
+// we need 6 test cases.
 let inputs = [
   ["add", 10, 20],
-  ["chair", 20, 10]
-]
+  ["chair", 20, 10],
+  ["sub", 10, 20],
+  ["mult", 10, 20],
+  [10, "mult", 20],
+  ["mult", 10, 20, 30, 40]
+];
 
-let outputs = [
-  30
-]
+let outputs = [30, undefined, -10, 200, undefined, undefined];
 
 /*
 The function takes an array. The array has length 3. The first element of the array is a string that represents an operation.
@@ -22,14 +24,36 @@ f(["spoof", 10, 10]); // undefined
 
 */
 function f(arr) {
-    
+  if (Array.isArray(arr) === false || arr.length !== 3) return undefined;
+
+  let op = arr[0];
+  let nA = arr[1];
+  let nB = arr[2];
+
+  if (
+    typeof op !== "string" ||
+    Number.isSafeInteger(nA) === false ||
+    Number.isSafeInteger(nB) === false
+  )
+    return undefined;
+
+  return op === "add"
+    ? nA + nB
+    : op === "sub"
+    ? nA - nB
+    : op === "mult"
+    ? nA * nB
+    : undefined;
 }
 
 function runTest(i) {
-    if(i > inputs.length) throw new Error("You do not have enough test cases");
-    let expected = outputs[i];
-    let actual = f(inputs[i]);
-    verifyEquals(expected, actual)
+  if (i > inputs.length) throw new Error("You do not have enough test cases");
+  let expected = outputs[i];
+  let actual = f(inputs[i]);
+
+  console.log(actual, expected);
+
+  verifyEquals(expected, actual);
 }
 
 runTest(0);
@@ -38,4 +62,4 @@ runTest(2);
 runTest(3);
 runTest(4);
 runTest(5);
-console.log("All tests passed for " + __filename)
+console.log("All tests passed for " + __filename);
