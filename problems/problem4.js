e20c996251229a27d7e3c75facc614d3fd0dcb88
JT <revirth@gmail.com>
2019-04-05 12:43:38 -0400

    problem4

--- problems/problem4.js
+++ problems/problem4.js
@@ -1,15 +1,18 @@
-let verifyEquals = require('./verify-equals.js');
+let verifyEquals = require("./verify-equals.js");
 
 // we need 8 test cases. I've provided the first 2
 let inputs = [
   ["hello", 4],
-  ["", 2]
-]
+  ["", 2],
+  ["abc", 0],
+  [0, "abc"],
+  [123, 2],
+  [verifyEquals, 2],
+  ["Hangry!!!!!", 0],
+  ["Hangry!!!!!", 0, 1, 2, 3, 99]
+];
 
-let outputs = [
-  "o",
-  undefined
-]
+let outputs = ["o", undefined, "a", undefined, undefined, undefined, "H", "H"];
 
 /*
 Make this function return the letter at the specified position in the string. If no such letter exists, it should return undefined.
@@ -21,14 +24,26 @@ f(["abc", 0]); // a
 
 */
 function f(arr) {
-    
+  // if (Array.isArray(arr) && arr.length !== 2) return undefined; // too strict ?
+
+  if (Array.isArray(arr) && arr.length < 2) return undefined;
+
+  let str = arr[0];
+  let idx = arr[1];
+
+  if (typeof str !== "string" || Number.isNaN(idx)) return undefined;
+
+  return str[idx];
 }
 
 function runTest(i) {
-    let expected = outputs[i];
-    let input = inputs[i];
-    let actual = f(input);
-    verifyEquals(expected, actual)
+  let expected = outputs[i];
+  let input = inputs[i];
+  let actual = f(input);
+
+  console.log(i, actual, expected, actual === expected);
+
+  verifyEquals(expected, actual);
 }
 
 runTest(0);
@@ -39,4 +54,4 @@ runTest(4);
 runTest(5);
 runTest(6);
 runTest(7);
-console.log("All tests passed for " + __filename)
+console.log("All tests passed for " + __filename);
