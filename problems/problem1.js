5dccaa281a044b42117ffdc50127696a5cf5e367
JT <revirth@gmail.com>
2019-04-05 11:41:36 -0400

    problem1 > add isLetter

--- problems/problem1.js
+++ problems/problem1.js
@@ -1,18 +1,28 @@
 let verifyEquals = require('./verify-equals.js'); // this line imports the verifyEquals function from the verify-equals.js file
 
 // we need 5 test cases. I provided 1 input
-let inputs = ['ab', "", " b"]
-
-let outputs = ['a', undefined, 'b']
+let inputs = ['ab', "", " b", "   e d", "?f"]
+let outputs = ['a', undefined, 'b', 'e', 'f']
+let isLetter = x => x.toLowerCase() !== x.toUpperCase();
 
 // Make this function return the first letter of the string that is passed to it. If the string does not have a first letter, return undefined
 function f(str) {
-    return str.trim()[0];
+
+    for (let i = 0; i < str.length; i++) {
+        const ch = str[i];
+
+        if( isLetter(ch) ) return ch;
+    }
+
+    return undefined;
 }
 
 function runTest(i) {
     let expected = outputs[i];
     let actual = f(inputs[i]);
+
+    console.log(actual, expected, actual === expected)
+
     verifyEquals(expected, actual) // verifyEquals is imported above
 }
 
